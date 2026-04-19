f2d5ac022dfb4c09fd5b8de021d1abf549d15707
Vlad Moisanu <vlad.moisanu@gmail.com>
2026-04-19 13:19:32 +0300

    Reworked GUI from QDialog to QMainWindow; Added Menu Bar

--- GUI.py
+++ GUI.py
@@ -1,27 +1,51 @@
 import sys
 
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QLineEdit, QToolButton, QDialog, QMainWindow, \
-    QVBoxLayout
+from PySide6.QtGui import QIcon, QAction, QGuiApplication
+from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QLineEdit, QToolButton, QMainWindow, \
+    QHBoxLayout, QWidget, QGraphicsView, QMenuBar
 from PySide6.QtCore import Slot
 
-class MainWindow(QDialog):
+class MainWindow(QMainWindow):
     def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
+        super().__init__(parent)
         self.setWindowTitle("Pixel Art Transformer")
-        self.setWindowIcon(QIcon("icon.png"))
-        #self.setStyleSheet("background-color: rgb(2, 25, 55);")
-        self.edit = QLineEdit("Write text here")
-        self.button = QPushButton("Click me")
-        self.label = QLabel("Hello World")
+        # noinspection PyTypeChecker
+        self.resize(QGuiApplication.primaryScreen().availableSize()*3/5)
+        self.createMenuBar()
 
-        self.button.clicked.connect(self.hello)
+        preview = QLabel("Preview")
+        uploadButton = QPushButton("Upload Image")
+        startButton = QPushButton("Start")
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.edit)
-        layout.addWidget(self.button)
-        layout.addWidget(self.label)
-        self.setLayout(layout)
+        layout = QHBoxLayout()
+        layout.addWidget(preview)
+        layout.addWidget(uploadButton)
+        layout.addWidget(startButton)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+    def createMenuBar(self):
+        menuBar = self.menuBar()
+
+        fileMenu = menuBar.addMenu("&File")
+        actionNew = QAction("New...", self)
+        actionOpen = QAction("Open...", self)
+        actionSave = QAction("Save...", self)
+        actionSaveAs = QAction("Save as...", self)
+
+        fileMenu.addAction(actionNew)
+        fileMenu.addAction(actionOpen)
+        fileMenu.addAction(actionSave)
+        fileMenu.addAction(actionSaveAs)
+
+        presetsMenu = menuBar.addMenu("&Presets")
+        actionVGA = QAction("VGA", self)
+        actionEGA = QAction("EGA", self)
+
+        presetsMenu.addAction(actionVGA)
+        presetsMenu.addAction(actionEGA)
 
     def hello(self):
         print(f"You wrote {self.edit.text()}")
@@ -29,7 +53,5 @@ class MainWindow(QDialog):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
-
-
     window.show()
     sys.exit(app.exec())
