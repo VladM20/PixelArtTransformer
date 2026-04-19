7e1b713b09b6d9b7c121b19c1d4bed80c5e84a6c
Vlad Moisanu <vlad.moisanu@gmail.com>
2026-04-19 17:36:16 +0300

    Added Save and Save As functionality

--- GUI.py
+++ GUI.py
@@ -1,11 +1,13 @@
 import sys
-
+import os
+from pathlib import Path
 from PySide6.QtCore import QSize, Slot
 
 import image_processing as image
 from PySide6.QtGui import QIcon, QAction, QGuiApplication, QPixmap, Qt, QImage
 from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QSlider, QSpinBox, QMainWindow, \
     QHBoxLayout, QWidget, QFileDialog, QComboBox, QVBoxLayout
+from PySide6.QtCore import QSettings
 
 
 class MainWindow(QMainWindow):
@@ -85,17 +87,20 @@ class MainWindow(QMainWindow):
         menuBar = self.menuBar()
 
         fileMenu = menuBar.addMenu("&File")
-        actionNew = QAction("New...", self)
         actionOpen = QAction("Open...", self)
         actionSave = QAction("Save...", self)
         actionSaveAs = QAction("Save as...", self)
 
-        fileMenu.addAction(actionNew)
         fileMenu.addAction(actionOpen)
         fileMenu.addAction(actionSave)
         fileMenu.addAction(actionSaveAs)
 
+        actionSave.triggered.connect(self.saveImage)
+        actionSave.setShortcut("Ctrl+S")
+        actionSaveAs.triggered.connect(self.saveAsImage)
+        actionSaveAs.setShortcut("Ctrl+Shift+S")
         actionOpen.triggered.connect(self.uploadImage)
+        actionOpen.setShortcut("Ctrl+O")
 
         presetsMenu = menuBar.addMenu("&Presets")
         actionVGA = QAction("VGA (256 colors)", self)
@@ -136,6 +141,52 @@ class MainWindow(QMainWindow):
         QImg = QImage(result.data, width, height, bytesPerLine, QImage.Format.Format_RGB888)
         pixmap = QPixmap.fromImage(QImg)
         self.preview.setPixmap(pixmap.scaled(self.preview.size(), Qt.AspectRatioMode.KeepAspectRatio))
+
+    @Slot()
+    def saveAsImage(self, setPreferences=False):
+        if self.preview.pixmap().isNull():
+            print("No Image Selected")
+            return
+        dir = str(Path(self.currentImage).stem) + "_pixelized"
+        filePath, filter = QFileDialog.getSaveFileName(
+            self,
+            "Save Image As",
+            dir,
+            "PNG (*.png);;JPEG (*.jpg *.jpeg);;BMP (*.bmp);;All Files (*.*)"
+        )
+        if filePath:
+            self.preview.pixmap().save(filePath)
+            # save default save directory and format
+            if setPreferences:
+                settings = QSettings("PixelArtTransformer", "Settings")
+
+                saveDirectory = Path(filePath).parent
+                settings.setValue("default_save_directory", saveDirectory)
+
+                saveFormat = Path(filePath).suffix
+                settings.setValue("default_save_format", saveFormat)
+
+
+    @Slot()
+    def saveImage(self):
+        if not self.currentImage or self.preview.pixmap().isNull():
+            print("No Image Selected")
+            return
+        # read preferences
+        settings = QSettings("PixelArtTransformer", "Settings")
+        defaultDirectory = settings.value("default_save_directory")
+        defaultFormat = settings.value("default_save_format")
+
+        if not defaultDirectory or not defaultFormat:
+            self.saveAsImage(setPreferences=True)
+            return
+
+        oldFileName = Path(self.currentImage).stem
+
+        newFileName = oldFileName + "_pixelized" + defaultFormat
+        fullPath = str(Path(defaultDirectory) / newFileName)
+
+        self.preview.pixmap().save(fullPath)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
