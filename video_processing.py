ba74b9726fd68e41fc2cdc134674ab3d2fd12248
Vlad Moisanu <vlad.moisanu@gmail.com>
2026-04-23 23:53:04 +0300

    Added working thread class

--- video_processing.py
+++ video_processing.py
@@ -1,5 +1,73 @@
 import cv2 as cv
 import numpy as np
+from PySide6.QtCore import QThread, Signal
+from moviepy import VideoFileClip
+import os
+import tempfile
+import image_processing as image
+
+class VideoProcessing(QThread):
+    progress = Signal(int)
+    finished = Signal(str)
+
+    def __init__(self, inputPath, outputPath, newWidth, newHeight, palette, maxColors):
+        super().__init__()
+        self.inputPath = inputPath
+        self.outputPath = outputPath
+        self.newWidth = newWidth
+        self.newHeight = newHeight
+        self.palette = palette
+        self.maxColors = maxColors
+
+        def run(self):
+            cap = cv.VideoCapture(self.inputPath)
+            fps = cap.get(cv.CAP_PROP_FPS)
+            totalFrames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+            width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+            height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+            noSound = os.path.join(tempfile.gettempdir(), "noSound.mp4")
+            fourcc = cv.VideoWriter.fourcc(*"mp4v")
+            out = cv.VideoWriter(noSound, fourcc, fps, (width, height))
+
+            frameCount = 0
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+                frame = image.downscale(frame, self.newWidth, self.newHeight)
+                frame =  image.colorProcessing(frame, self.palette, self.maxColors)
+                frame = image.upscale(frame, width, height)
+                out.write(frame)
+
+                frameCount += 1
+                # progress bar calculation
+                # 20% is reserved for audio
+                percentage = int(frameCount / totalFrames * 80)
+                self.progress.emit(percentage)
+
+            cap.release()
+            out.release()
+
+            self.progress.emit(85)
+            originalClip = VideoFileClip(self.inputPath)
+            processedClip = VideoFileClip(noSound)
+
+            if originalClip.audio is not None:
+                processedClip.audio = originalClip.audio
+                processedClip.write_videofile(self.outputPath, codec="libx264", audio_codec="aac")
+            else:
+                import shutil
+                shutil.copy(out, self.outputPath)
+
+            originalClip.close()
+            processedClip.close()
+            os.remove(noSound)
+
+            self.progress.emit(100)
+            self.finished.emit(self.outputPath)
+
 
 def getFirstValidFrame(videoPath):
     cap = cv.VideoCapture(videoPath)
@@ -15,3 +83,4 @@ def getFirstValidFrame(videoPath):
 
     cap.release()
     return validFrame
+
